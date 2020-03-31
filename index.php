b9a699b6d06a88c93ad1fa5d2fb4ed8aa26a04fa
SrDonEmilio <emilio@nextup.dev>
2020-03-30 21:33:25 -0600

    Add static php version files

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- COMMON TAGS -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">    
+    <title>NextUp.dev</title>
+    <!-- Search Engine -->
+    <meta name="description" content="Software Development">
+    <meta name="image" content="https://nextup.dev/preview.jpg">
+    <!-- Schema.org for Google -->
+    <meta itemprop="name" content="NextUp.dev">
+    <meta itemprop="description" content="Software Development">
+    <meta itemprop="image" content="https://nextup.dev/preview.jpg">
+    <!-- Twitter -->
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:title" content="NextUp.dev">
+    <meta name="twitter:description" content="Software Development">
+    <meta name="twitter:image:src" content="https://nextup.dev/preview.jpg">
+    <!-- Open Graph general (Facebook, Pinterest & Google+) -->
+    <meta name="og:title" content="NextUp.dev">
+    <meta name="og:description" content="Software Development">
+    <meta name="og:image" content="https://nextup.dev/preview.jpg">
+    <meta name="og:url" content="https://nextup.dev">
+    <meta name="og:site_name" content="NextUp.dev">
+    <meta name="og:locale" content="es_MX">
+    <meta name="og:type" content="website">
+    <!-- Styles -->
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <!-- Scripts -->
+    <script src="js/three.r92.min.js"></script>
+    <script src="js/vanta.net.min.js"></script>
+
+</head>
+<body>
+    <div id="background"></div>
+    <div id="wrapper">
+        <div id="head">
+            <h1>NextUp.dev</h1>
+            <h2>Software Development</h2>
+            <div class="button" id="contact"><a href="mailto:contacto@nextup.dev">Contacto</a></div>
+        </div>
+    </div>
+    <script>
+        VANTA.NET({
+        el: "#background",
+        color: 0x3c76ca,
+        backgroundColor: 0x171220,
+        points: 8.00,
+        maxDistance: 25.00,
+        spacing: 13.00
+        })
+    </script>
+</body>
+</html>
