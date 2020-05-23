3c2bc0f31b87c5cc84d0a41dbec6ee9f8eaca439
SrDonEmilio <emilio@nextup.dev>
2020-05-23 01:29:18 -0500

    Supercomputer project added

--- src/containers/Portfolio.js
+++ src/containers/Portfolio.js
@@ -4,6 +4,7 @@ import Project from '../components/Project'
 import '../css/Portfolio.css'
 
 import myFavorites_preview from '../img/projects-preview/myfavorite_preview.jpg'
+import supercomputers from '../img/projects-preview/supercomputers.jpg'
 
 const Portfolio = () => (
     <div id="portfolio">
@@ -14,6 +15,13 @@ const Portfolio = () => (
             description="Simple application to show my favorite movies and TV shows, getting the information from TheMovieDB API"
             preview={myFavorites_preview}
             link="https://myfavorites.nextup.dev"
+        />
+        <Project 
+            name="supercomputers"
+            title="Supercomputers"
+            description="Ranking of the most powerful supercomputers in the World"
+            preview={supercomputers}
+            link="http://supercomputers.nextup.dev"
         />
     </div>
 )
